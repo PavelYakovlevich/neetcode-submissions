ec70b3f794693420bf932e163f54ce4f7185b732
Pavel Yakovlevich <33180686+PavelYakovlevich@users.noreply.github.com>
2026-07-16 16:46:33 +0200

    Add: take-gifts-from-the-richest-pile - submission-2

---
+++ Data Structures & Algorithms/take-gifts-from-the-richest-pile/submission-2.py
@@ -0,0 +1,10 @@
+import math
+class Solution:
+    def pickGifts(self, gifts: List[int], k: int) -> int:
+        heapq.heapify_max(gifts)
+        
+        for i in range(k):
+            pile = heapq.heappop_max(gifts)
+            heapq.heappush_max(gifts, math.floor(math.sqrt(pile)))
+        
+        return sum(gifts)
