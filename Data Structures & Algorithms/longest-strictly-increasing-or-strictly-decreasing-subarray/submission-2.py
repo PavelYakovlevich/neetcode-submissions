1b1ce1a9faedeb2877a0cc7c3a1549277015cb71
Pavel Yakovlevich <33180686+PavelYakovlevich@users.noreply.github.com>
2026-08-27 19:52:15 +0200

    Add: longest-strictly-increasing-or-strictly-decreasing-subarray - submission-2

---
+++ Data Structures & Algorithms/longest-strictly-increasing-or-strictly-decreasing-subarray/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestMonotonicSubarray(self, nums: List[int]) -> int:
+        res = 1
+        inc_len = dec_len = 1
+
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i - 1]:
+                inc_len += 1
+                dec_len = 1
+            elif nums[i] < nums[i - 1]:
+                dec_len += 1
+                inc_len = 1
+            else:
+                inc_len = dec_len = 1
+            
+            res = max(res, inc_len, dec_len)
+        
+        return res
