d29377565ecc269db9d9dd8c281888699d81456c
Pavel Yakovlevich <33180686+PavelYakovlevich@users.noreply.github.com>
2026-09-15 01:54:45 +0200

    Add: lemonade-change - submission-4

---
+++ Data Structures & Algorithms/lemonade-change/submission-4.py
@@ -0,0 +1,19 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        cash = defaultdict(int)
+
+        for bill in bills:
+            cash[bill] += 1
+            change = bill - 5
+
+            if change >= 10 and cash[10]:
+                change -= 10
+                cash[10] -= 1
+            
+            if change > 0:
+                cash[5] -= change // 5
+            
+            if cash[5] < 0:
+                return False
+
+        return True
